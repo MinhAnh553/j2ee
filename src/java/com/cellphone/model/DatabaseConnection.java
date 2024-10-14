1165d032c10a5eef27fbe53e33fc0c56b7994c3a
Nguyen Gia Bao <nggiabao19@gmail.com>
2024-10-14 11:48:48 +0700

    file admin

--- src/java/com/cellphone/model/DatabaseConnection.java
+++ src/java/com/cellphone/model/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/cellphone";
+    private static final String URL = "jdbc:mysql://localhost:3306/funphone";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
